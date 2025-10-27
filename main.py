ec51adfdd66cd35385504c0b0c38592b9e54e32d
Vincent W. <pc2025weingaertner@gmail.com>
2025-10-27 16:51:00 +0100

    created new picture

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import turtle
+import turtle as pen
 import random
 
-canvas = turtle.Screen()
-canvas.screensize(400, 400, "#C20C1E")
-turtle.shape("turtle")
-turtle.color('brown')
-turtle.pencolor('brown')
-turtle.speed(10000000000000000000000000000000000000000000000000000000000000000000000)
 
-turtle.pendown()
-list_of_colors = ['red', 'orange', 'yellow']
-for i in range(1000):
-    turtle.teleport(random.randint(-300, 300), random.randint(-300, 300))
-    turtle.pencolor(random.choice(list_of_colors))
-    random_stepsize = random.randint(1, 350)
-    for i in range(4):
-        turtle.forward(random_stepsize)
-        turtle.left(90)
+canvas = pen.Screen()
+canvas.screensize(400, 400, "white")
+pen.shape("turtle")
+pen.color('black')
+pen.pencolor('black')
+pen.speed(100)
+pen.pendown()
+list_of_colors = ['cyan', 'green']
 
+# for i in range(1000):
+#     turtle.teleport(random.randint(-300, 300), random.randint(-300, 300))
+#     random_stepsize = random.randint(20, 150)
+#     for i in range(1):
+#         turtle.circle(random_stepsize)
 
+size = 1
+for i in range (0,100):
+    pen.pencolor(random.choice(list_of_colors))
+    pen.circle(size)
+    pen.left(5)
+    size = size + 10
 
+for i in range (0,100):
+    pen.pencolor(random.choice(list_of_colors))
+    pen.circle(size)
+    pen.right(7)
+    size = size - 10
 
-turtle.teleport(150, 150)
-turtle.done()
-turtle.penup()
+pen.teleport(0, 0)
+pen.done()
+pen.penup()
 
-turtle.mainloop()
+pen.mainloop()
 
 
 

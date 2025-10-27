7e6e5b68466317de131a25983f728d50a97de967
Vincent W. <pc2025weingaertner@gmail.com>
2025-10-27 16:22:28 +0100

    created abstract random picture

--- main.py
+++ main.py
@@ -2,26 +2,22 @@ import turtle
 import random
 
 canvas = turtle.Screen()
-canvas.screensize(300, 400, "brown")
+canvas.screensize(400, 400, "#C20C1E")
 turtle.shape("turtle")
 turtle.color('brown')
 turtle.pencolor('brown')
-turtle.speed(1)
+turtle.speed(10000000000000000000000000000000000000000000000000000000000000000000000)
 
 turtle.pendown()
 list_of_colors = ['red', 'orange', 'yellow']
 for i in range(1000):
     turtle.teleport(random.randint(-300, 300), random.randint(-300, 300))
     turtle.pencolor(random.choice(list_of_colors))
-    for i in range(1):
-        turtle.forward(100)
+    random_stepsize = random.randint(1, 350)
+    for i in range(4):
+        turtle.forward(random_stepsize)
         turtle.left(90)
-        turtle.forward(100)
-        turtle.left(90)
-        turtle.forward(100)
-        turtle.left(90)
-        turtle.forward(100)
-        turtle.left(90)
+
 
 
 
@@ -30,6 +26,8 @@ turtle.done()
 turtle.penup()
 
 turtle.mainloop()
+
+
 
 
 
